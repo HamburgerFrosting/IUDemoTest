0175d02ab2699eed3132cab09cf38d9b6ceeef00
HamburgerFrosting <140783869+HamburgerFrosting@users.noreply.github.com>
2025-05-14 23:27:52 -0400

    Add copyright notice

--- scripts/changemain.js
+++ scripts/changemain.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2022-2025 HamburgerFrosting
+ * Licensed under the MIT license
+ */
+
 function changeMain(number) {
     let main = document.getElementById('main');
     if (number == 0) {
